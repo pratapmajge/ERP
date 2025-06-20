27b2897bb866ef15b65cc5ff37cf2fc4150f715d
pratapmajge <majgepratap1504@gmail.com>
2025-06-20 17:02:47 +0530

    increased limit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,7 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000 // increase the limit from default 500kb to 2000kb (or any value you want)
+  }
 })
