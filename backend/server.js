5273c08e7d21bd21d6eee855d73973b74a2d4b10
pratapmajge <majgepratap1504@gmail.com>
2025-06-20 17:46:16 +0530

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -15,18 +15,7 @@ connectDB();
 const app = express();
 
 // Middleware
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
